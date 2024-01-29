e9aa48c829562153cd142d9ac6b921d1332e6181
ogb <b@faceted.ai>
2024-01-29 10:09:42 +0000

    run dpo

--- dpo/dpo_mistral.py
+++ dpo/dpo_mistral.py
@@ -24,173 +24,193 @@ wb_token = os.getenv("wandb_token")
 wandb.login(key=wb_token)
 login(token=hf_token)
 
+run = wandb.init(
+    project="dpo-35-v0",  # Project name.
+    name="log model end",          # name of the run within this project.
+    config={                     # Configuration dictionary.
+        "split": "train"
+    },
+    group="fafo",             # Group runs. This run belongs in "dataset".
+    tags=["end"],            # Tags. More dynamic, low-level grouping.
+    notes=""  # Description about the run.
+)  # Check out the
 
-# model_name = "teknium/OpenHermes-2.5-Mistral-7B"
-# new_model = "NeuralHermes-2.5-Mistral-7B"
+# Log model when running HF Trainer reporting to wandb.
+os.environ["WANDB_LOG_MODEL"] = "end"  # Apparently this is deprecated in version 5 of transformers.
 
-# # chatml like OG Mistral
-# tokeniser_model_name = "teknium/OpenHermes-2.5-Mistral-7B"
-# """## Format dataset"""
+# Use wandb to watch the gradients & model parameters.
+os.environ["WANDB_WATCH"] = "all"
 
-# def chatml_format(example):
-#     # Format system
-#     if len(example['system']) > 0:
-#         message = {"role": "system", "content": example['system']}
-#         system = tokenizer.apply_chat_template([message], tokenize=False)
-#     else:
-#         system = ""
+print("wandb log model:", os.environ.get("WANDB_LOG_MODEL"))
 
-#     # Format instruction
-#     message = {"role": "user", "content": example['question']}
-#     prompt = tokenizer.apply_chat_template([message], tokenize=False, add_generation_prompt=True)
+model_name = "ogbrandt/mistral-7b-non-instruction-pjf-ft"
+new_model = "mistral-7b-non-instruction-pjf-ft-gpt35-dpo-v0"
 
-#     # Format chosen answer
-#     chosen = example['chatgpt'] + "<|im_end|>\n"
+# chatml like OG Mistral
+tokeniser_model_name = "teknium/OpenHermes-2.5-Mistral-7B"
+"""## Format dataset"""
 
-#     # Format rejected answer
-#     rejected = example['llama2-13b-chat'] + "<|im_end|>\n"
+def chatml_format(example):
+    # # Format system
+    # if len(example['system']) > 0:
+    #     message = {"role": "system", "content": example['system']}
+    #     system = tokenizer.apply_chat_template([message], tokenize=False)
+    # else:
+    #     system = ""
 
-#     return {
-#         "prompt": system + prompt,
-#         "chosen": chosen,
-#         "rejected": rejected,
-#     }
+    system_prompt = ""
 
-# # Load dataset
-# dataset = load_dataset("Intel/orca_dpo_pairs")['train']
+    # Format instruction
+    message = {"role": "user", "content": example['prompt']}
+    prompt = tokenizer.apply_chat_template([message], tokenize=False, add_generation_prompt=True)
 
-# # Save columns
-# original_columns = dataset.column_names
+    # Format chosen answer
+    chosen = example['chosen'] + "<|im_end|>\n"
 
-# # Tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# tokenizer.pad_token = tokenizer.eos_token
-# tokenizer.padding_side = "left"
+    # Format rejected answer
+    rejected = example['rejected'] + "<|im_end|>\n"
 
-# # Format dataset
-# dataset = dataset.map(
-#     chatml_format,
-#     remove_columns=original_columns
-# )
+    return {
+        "prompt": system_prompt + prompt,
+        "chosen": chosen,
+        "rejected": rejected,
+    }
 
-# # Print sample
-# dataset[1]
+# Load dataset
+dataset = load_dataset("ogbrandt/gpt35_preference_rlaif")['train']
 
-# """## Train model with DPO"""
+# Save columns
+original_columns = dataset.column_names
 
-# # LoRA configuration
-# peft_config = LoraConfig(
-#     r=16,
-#     lora_alpha=16,
-#     lora_dropout=0.05,
-#     bias="none",
-#     task_type="CAUSAL_LM",
-#     target_modules=['k_proj', 'gate_proj', 'v_proj', 'up_proj', 'q_proj', 'o_proj', 'down_proj']
-# )
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
 
-# # Model to fine-tune
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     torch_dtype=torch.float16,
-#     load_in_4bit=True
-# )
-# model.config.use_cache = False
+# Format dataset
+dataset = dataset.map(
+    chatml_format,
+    remove_columns=original_columns
+)
 
-# # Reference model
-# ref_model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     torch_dtype=torch.float16,
-#     load_in_4bit=True
-# )
+# Print sample
+dataset[1]
 
-# # Training arguments
-# training_args = TrainingArguments(
-#     per_device_train_batch_size=4,
-#     gradient_accumulation_steps=4,
-#     gradient_checkpointing=True,
-#     learning_rate=5e-5,
-#     lr_scheduler_type="cosine",
-#     max_steps=200,
-#     save_strategy="no",
-#     logging_steps=1,
-#     output_dir=new_model,
-#     optim="paged_adamw_32bit",
-#     warmup_steps=100,
-#     bf16=True,
-#     report_to="wandb",
-# )
+"""## Train model with DPO"""
 
-# # Create DPO trainer
-# dpo_trainer = DPOTrainer(
-#     model,
-#     ref_model,
-#     args=training_args,
-#     train_dataset=dataset,
-#     tokenizer=tokenizer,
-#     peft_config=peft_config,
-#     beta=0.1,
-#     max_prompt_length=1024,
-#     max_length=1536,
-# )
+# LoRA configuration
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=16,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=['k_proj', 'gate_proj', 'v_proj', 'up_proj', 'q_proj', 'o_proj', 'down_proj']
+)
 
-# # Fine-tune model with DPO
-# dpo_trainer.train()
+# Model to fine-tune
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.float16,
+    load_in_4bit=True
+)
+model.config.use_cache = False
 
-# """## Upload model"""
+# Reference model
+ref_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.float16,
+    load_in_4bit=True
+)
 
-# # Save artifacts
-# dpo_trainer.model.save_pretrained("final_checkpoint")
-# tokenizer.save_pretrained("final_checkpoint")
+# Training arguments
+training_args = TrainingArguments(
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    gradient_checkpointing=True,
+    learning_rate=5e-5,
+    lr_scheduler_type="cosine",
+    max_steps=200,
+    save_strategy="no",
+    logging_steps=1,
+    output_dir=new_model,
+    optim="paged_adamw_32bit",
+    warmup_steps=100,
+    bf16=True,
+    report_to="wandb",
+)
 
-# # Flush memory
-# del dpo_trainer, model, ref_model
-# gc.collect()
-# torch.cuda.empty_cache()
+# Create DPO trainer
+dpo_trainer = DPOTrainer(
+    model,
+    ref_model,
+    args=training_args,
+    train_dataset=dataset,
+    tokenizer=tokenizer,
+    peft_config=peft_config,
+    beta=0.1,
+    max_prompt_length=1024,
+    max_length=1536,
+)
 
-# # Reload model in FP16 (instead of NF4)
-# base_model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     return_dict=True,
-#     torch_dtype=torch.float16,
-# )
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Fine-tune model with DPO
+dpo_trainer.train()
 
-# # Merge base model with the adapter
-# model = PeftModel.from_pretrained(base_model, "final_checkpoint")
-# model = model.merge_and_unload()
+"""## Upload model"""
 
-# # Save model and tokenizer
-# model.save_pretrained(new_model)
-# tokenizer.save_pretrained(new_model)
+# Save artifacts
+dpo_trainer.model.save_pretrained("final_checkpoint")
+tokenizer.save_pretrained("final_checkpoint")
 
-# # Push them to the HF Hub
-# model.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
-# tokenizer.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
+# Flush memory
+del dpo_trainer, model, ref_model
+gc.collect()
+torch.cuda.empty_cache()
 
-# """## Inference"""
+# Reload model in FP16 (instead of NF4)
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    return_dict=True,
+    torch_dtype=torch.float16,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# # Format prompt
-# message = [
-#     {"role": "system", "content": "You are a helpful assistant chatbot."},
-#     {"role": "user", "content": "What is a Large Language Model?"}
-# ]
-# tokenizer = AutoTokenizer.from_pretrained(new_model)
-# prompt = tokenizer.apply_chat_template(message, add_generation_prompt=True, tokenize=False)
+# Merge base model with the adapter
+model = PeftModel.from_pretrained(base_model, "final_checkpoint")
+model = model.merge_and_unload()
 
-# # Create pipeline
-# pipeline = transformers.pipeline(
-#     "text-generation",
-#     model=new_model,
-#     tokenizer=tokenizer
-# )
+# Save model and tokenizer
+model.save_pretrained(new_model)
+tokenizer.save_pretrained(new_model)
 
-# # Generate text
-# sequences = pipeline(
-#     prompt,
-#     do_sample=True,
-#     temperature=0.7,
-#     top_p=0.9,
-#     num_return_sequences=1,
-#     max_length=200,
-# )
-# print(sequences[0]['generated_text'])
+# Push them to the HF Hub
+model.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
+tokenizer.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
+
+"""## Inference"""
+
+# Format prompt
+message = [
+    {"role": "system", "content": "You are a helpful assistant chatbot."},
+    {"role": "user", "content": "What is a Large Language Model?"}
+]
+tokenizer = AutoTokenizer.from_pretrained(new_model)
+prompt = tokenizer.apply_chat_template(message, add_generation_prompt=True, tokenize=False)
+
+# Create pipeline
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=new_model,
+    tokenizer=tokenizer
+)
+
+# Generate text
+sequences = pipeline(
+    prompt,
+    do_sample=True,
+    temperature=0.7,
+    top_p=0.9,
+    num_return_sequences=1,
+    max_length=200,
+)
+print(sequences[0]['generated_text'])
